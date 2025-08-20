52162b6ae5a1ca939d6b9c22498f5acf23ea7391
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-08-20 01:52:42 -0300

    feat(user): adiciona UserController com endpoint de criação

---
+++ src/main/java/br/com/hebertesteves/todolist/user/UserController.java
@@ -0,0 +1,38 @@
+package br.com.hebertesteves.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador:
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiro
+     * Double (double) numeros 0.0000
+     * Float (float) numeros 0.000
+     * char (A C)
+     * Date (data)
+     * void
+     */
+
+    /**
+     * Body
+     */
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
+    }
+}
